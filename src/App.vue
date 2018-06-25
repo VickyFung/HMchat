79dab4173f2b89c5e7cbc2d2dc6779a6d7091047
VickyFung <18817392508@163.com>
2018-06-25 17:42:48 +0800

    first commit of HMchat

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+       这是App组件
+    <mt-header fixed title="VickyFung VuePgrm"></mt-header>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+
+</style>
